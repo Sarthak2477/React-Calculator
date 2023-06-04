51405d87e01b5f2944e1efd091249c4510f822fa
sarthak pawar <64775083+Sarthak2477@users.noreply.github.com>
2023-06-04 08:15:56 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="calc_grid">
+      <div className="prev-op"></div>
+      <div className="curr_op"></div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>+</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button></button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
